4488084de33a3ced7bbd05d66558ba3fbe5638f5
Rebeca Cabañas Arauz <beckyarauz@users.noreply.github.com>
2018-12-04 18:33:30 +0100

    Updates

---
+++ precache-manifest.37e7d0e7f256593e9d1b9d3a1132a866.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba66877ca657acde70640d4aa9990a62",
+    "url": "/Coffee-Shop-React/static/media/coffeegrains.ba66877c.jpg"
+  },
+  {
+    "revision": "ae3c1e65cb89b63bffc8",
+    "url": "/Coffee-Shop-React/static/css/main.91e38738.chunk.css"
+  },
+  {
+    "revision": "b80ee9f5381ac540c3bb",
+    "url": "/Coffee-Shop-React/static/js/1.b80ee9f5.chunk.js"
+  },
+  {
+    "revision": "26739a9c4e5272bc85f9",
+    "url": "/Coffee-Shop-React/static/js/runtime~main.26739a9c.js"
+  },
+  {
+    "revision": "99daae208f0fa446c5be638846179467",
+    "url": "/Coffee-Shop-React/static/media/logo.99daae20.svg"
+  },
+  {
+    "revision": "81323388b0a0234ba0acfe3126364a5f",
+    "url": "/Coffee-Shop-React/static/media/quijote-logo.81323388.png"
+  },
+  {
+    "revision": "23d9c04f1516fd6335f95c2ed76db984",
+    "url": "/Coffee-Shop-React/static/media/riders-logo.23d9c04f.png"
+  },
+  {
+    "revision": "53dcb35bfaafd2d1aeefe383aad218f0",
+    "url": "/Coffee-Shop-React/static/media/concrete1.53dcb35b.jpg"
+  },
+  {
+    "revision": "213279469a4ded6d8d2c7c7ab98f8c23",
+    "url": "/Coffee-Shop-React/static/media/PlaceOutsideFront.21327946.jpg"
+  },
+  {
+    "revision": "2d431135a514dda50bb5df3df071a776",
+    "url": "/Coffee-Shop-React/static/media/inside.2d431135.JPG"
+  },
+  {
+    "revision": "09bea0cd775fed2b4acc1f8f1e3e61c2",
+    "url": "/Coffee-Shop-React/static/media/concrete.09bea0cd.png"
+  },
+  {
+    "revision": "ecc382634ecd37807f44a6492894ae2d",
+    "url": "/Coffee-Shop-React/static/media/sorrento.ecc38263.png"
+  },
+  {
+    "revision": "3292b5b0b08ef6fae50c7c34e85bb85d",
+    "url": "/Coffee-Shop-React/static/media/FoodPeanuts.3292b5b0.jpg"
+  },
+  {
+    "revision": "4d20c277639102a7dc2b97ea9e641dde",
+    "url": "/Coffee-Shop-React/static/media/FoodWoodBread.4d20c277.jpg"
+  },
+  {
+    "revision": "2e19165ab5ba82cc91b2c4ed3f56152b",
+    "url": "/Coffee-Shop-React/static/media/FoodWoodLoafTable.2e19165a.jpg"
+  },
+  {
+    "revision": "4e9e206a8b295631484ea71354f6ea08",
+    "url": "/Coffee-Shop-React/static/media/PlaceOutside.4e9e206a.jpg"
+  },
+  {
+    "revision": "d8cafd5c6efad9468d8717e17ac6fcc5",
+    "url": "/Coffee-Shop-React/static/media/cafewood.d8cafd5c.jpg"
+  },
+  {
+    "revision": "a165f8c43503533b571fe3ef7e74685f",
+    "url": "/Coffee-Shop-React/static/media/caffe2.a165f8c4.jpg"
+  },
+  {
+    "revision": "49061e65a0f4be9f9f137fef1edbe726",
+    "url": "/Coffee-Shop-React/static/media/caffetable.49061e65.jpg"
+  },
+  {
+    "revision": "3852dae63df70389b300f9c58f788de8",
+    "url": "/Coffee-Shop-React/static/media/caffewild4.3852dae6.jpg"
+  },
+  {
+    "revision": "bd22a3be8874e650de7f71f9e5cd4839",
+    "url": "/Coffee-Shop-React/static/media/coffeeAutumn.bd22a3be.jpg"
+  },
+  {
+    "revision": "3693bf28c03355b7eeac94dc7f538e95",
+    "url": "/Coffee-Shop-React/static/media/coffeeBlueWoodChair.3693bf28.jpg"
+  },
+  {
+    "revision": "e857ecf4cd26d08da49b3ead10f5b4ee",
+    "url": "/Coffee-Shop-React/static/media/coffeeStool.e857ecf4.jpg"
+  },
+  {
+    "revision": "ae3c1e65cb89b63bffc8",
+    "url": "/Coffee-Shop-React/static/js/main.ae3c1e65.chunk.js"
+  },
+  {
+    "revision": "751883f415ce18db4350df0a9264c728",
+    "url": "/Coffee-Shop-React/static/media/coffeeroundtable.751883f4.jpg"
+  },
+  {
+    "revision": "ad8e5d20b9bb6a5cb57fb04a462a0d50",
+    "url": "/Coffee-Shop-React/static/media/coffeewild1.ad8e5d20.jpg"
+  },
+  {
+    "revision": "511ffec1cdc62fc62c803ba0cba9042d",
+    "url": "/Coffee-Shop-React/static/media/coffeewild2.511ffec1.jpg"
+  },
+  {
+    "revision": "e8115de21dc63f5e56619df1dc6039b0",
+    "url": "/Coffee-Shop-React/static/media/coffeewild3.e8115de2.jpg"
+  },
+  {
+    "revision": "40c5776c63ccca742afbe08086ad0d2a",
+    "url": "/Coffee-Shop-React/static/media/coffeewild5.40c5776c.jpg"
+  },
+  {
+    "revision": "182204b22288201d33ef61ebd52700fe",
+    "url": "/Coffee-Shop-React/static/media/coffeewild6.182204b2.jpg"
+  },
+  {
+    "revision": "5056a992cc51c3615eddf14c1268bef5",
+    "url": "/Coffee-Shop-React/static/media/coffeewild6v2.5056a992.jpg"
+  },
+  {
+    "revision": "0ab009ae1b8ea3b1783ea7dc498df13f",
+    "url": "/Coffee-Shop-React/static/media/coffeewinter.0ab009ae.jpg"
+  },
+  {
+    "revision": "d05baa0db1b58a1bc8b6bf2699b12228",
+    "url": "/Coffee-Shop-React/static/media/coffeework.d05baa0d.jpg"
+  },
+  {
+    "revision": "12bd03fbd775099d8ebd6dd92bbd6152",
+    "url": "/Coffee-Shop-React/static/media/foodHams.12bd03fb.jpg"
+  },
+  {
+    "revision": "89387de855cc06ccf453a0200fc699fb",
+    "url": "/Coffee-Shop-React/static/media/foodwild1.89387de8.jpg"
+  },
+  {
+    "revision": "eeb442196c7013bd647a583773094f5b",
+    "url": "/Coffee-Shop-React/static/media/foodwild10.eeb44219.jpg"
+  },
+  {
+    "revision": "1788a9e8ea136db904a169d105eedfbe",
+    "url": "/Coffee-Shop-React/static/media/foodwild2.1788a9e8.jpg"
+  },
+  {
+    "revision": "5a722e0743a876b30bf4e049d288dd4f",
+    "url": "/Coffee-Shop-React/static/media/foodwild3.5a722e07.jpg"
+  },
+  {
+    "revision": "aed42815fcf1f5d52e9a18fcf46b4a37",
+    "url": "/Coffee-Shop-React/static/media/foodwild4.aed42815.jpg"
+  },
+  {
+    "revision": "6f7f8f91f0b7fb58d7620168b08eb378",
+    "url": "/Coffee-Shop-React/static/media/foodwild5.6f7f8f91.jpg"
+  },
+  {
+    "revision": "c7fa8e32c8d2335c65d156eb57fab767",
+    "url": "/Coffee-Shop-React/static/media/foodwild6.c7fa8e32.jpg"
+  },
+  {
+    "revision": "f96784e332994619478bcb9da5c931b0",
+    "url": "/Coffee-Shop-React/static/media/foodwild7.f96784e3.jpg"
+  },
+  {
+    "revision": "3bd798290e1a5f6e94cd5d28b6676e9d",
+    "url": "/Coffee-Shop-React/static/media/foodwild8.3bd79829.jpg"
+  },
+  {
+    "revision": "374078d32d617f114c25fe33ef3b622e",
+    "url": "/Coffee-Shop-React/static/media/foodwild9.374078d3.jpg"
+  },
+  {
+    "revision": "ab31fa80aa4254035e1e06722b5edf05",
+    "url": "/Coffee-Shop-React/static/media/wildfrontwindow.ab31fa80.jpg"
+  },
+  {
+    "revision": "711b32428ae868ab9dd37d7d49222ed3",
+    "url": "/Coffee-Shop-React/index.html"
+  }
+];
